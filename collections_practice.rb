4ea42bcb6d3498adf338ec038f2712913a3bb8fa
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-15 06:38:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,7 +12,7 @@ def sort_array_char_count(arrays)
   arrays.sort_by {|arrays| arrays.length}
 end
 
-def swap_element(array)
+def swap_elements(array)
   first_variable = array [1]
   second_variable = array [2]
   array [1] = second_variable
